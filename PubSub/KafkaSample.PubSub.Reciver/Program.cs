2e2c0fc52a419bfe905e9c713b5a8bcc50c4f521
Adbolghafar <Adbolghafar@>
2024-12-27 22:12:56 +0330

    Add project files.

---
+++ PubSub/KafkaSample.PubSub.Reciver/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Confluent.Kafka;
+
+Console.WriteLine("Consumer Config!");
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092", // Kafka broker address
+    GroupId = "test-consumer-group",     // Consumer group ID
+    AutoOffsetReset = AutoOffsetReset.Earliest, // Start reading from the beginning if no offset is found
+    EnableAutoCommit = true,
+    SecurityProtocol = SecurityProtocol.SaslPlaintext, // Use SASL_PLAINTEXT
+    SaslMechanism = SaslMechanism.Plain, // Use PLAIN mechanism
+    SaslUsername = "admin",             // Replace with your username
+    SaslPassword = "123"// Automatically commit offsets
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("test-topic");
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) => {
+        e.Cancel = true; // Prevent default termination
+        cts.Cancel();
+    };
+
+    try
+    {
+        while (true)
+        {
+            var consumeResult = consumer.Consume(cts.Token);
+            Console.WriteLine($"Consumed message '{consumeResult.Value}' at: '{consumeResult.TopicPartitionOffset}'.");
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        // Exit gracefully on Ctrl+C
+    }
+    finally
+    {
+        consumer.Close(); // Ensure offsets are committed before exiting
+    }
+}
+
---
+++ PubSub/KafkaSample.PubSub.Sender/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+Console.WriteLine("Producer Config");
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",// Kafka broker address
+    SecurityProtocol = SecurityProtocol.SaslPlaintext, // Use SASL_PLAINTEXT
+    SaslMechanism = SaslMechanism.Plain, // Use PLAIN mechanism
+    SaslUsername = "admin",             // Replace with your username
+    SaslPassword = "123"
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    try
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var message = new Message<Null, string> { Value = $"Message {i}" };
+
+            var deliveryResult = await producer.ProduceAsync("test-topic", message);
+
+            Console.WriteLine($"Delivered '{deliveryResult.Value}' to '{deliveryResult.TopicPartitionOffset}'");
+        }
+    }
+    catch (ProduceException<Null, string> ex)
+    {
+        Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+    }
+}
+
+Console.ReadLine();
